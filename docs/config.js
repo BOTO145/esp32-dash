848aaea2fd7f0c714743fb52f9448cca98f1ac1f
BOTO145 <155305869+BOTO145@users.noreply.github.com>
2024-11-28 20:45:45 +0530

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,7 @@
 // Configuration
 const CONFIG = {
     // Replace with your actual Render.com WebSocket URL
-    WEBSOCKET_URL: 'wss://esp32-dash-ws.onrender.com'
+    WEBSOCKET_URL: 'wss://esp32-dash.onrender.com',
+    RECONNECT_TIMEOUT: 5000,
+    MAX_TABLE_ROWS: 100
 };
